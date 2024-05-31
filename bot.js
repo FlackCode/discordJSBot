3f2e05ef16bb2280ee8b8b1093374f297da98219
flack <163582560+FlackCode@users.noreply.github.com>
2024-05-31 21:15:55 +0200

    Added 2 new commands!
    added command for hello and for repeating what a user said. learned useroptions and stringoptions

--- bot.js
+++ bot.js
@@ -12,9 +12,30 @@ client.on('ready', (c) => {
     const hello = new SlashCommandBuilder()
       .setName('hello')
       .setDescription('Says hello to someone')
-    
+      .addUserOption(option => 
+        option
+            .setName('user')
+            .setDescription('User to say hi to')
+            .setRequired(false)
+      )
+      .addStringOption(option => 
+        option
+            .setName('text')
+            .setDescription('Add text to the message!')
+            .setRequired(false)
+      )
+    const echo = new SlashCommandBuilder()
+      .setName('echo')
+      .setDescription('Repeats what you said')
+      .addStringOption(option => 
+        option
+            .setName('text')
+            .setDescription('The text to repeat')
+            .setRequired(true)
+      )
     client.application.commands.create(ping, process.env.GUILD_ID)
     client.application.commands.create(hello, process.env.GUILD_ID)
+    client.application.commands.create(echo, process.env.GUILD_ID)
 })
 
 client.on('interactionCreate', interaction => {
@@ -23,7 +44,13 @@ client.on('interactionCreate', interaction => {
         interaction.reply('Pong!')
     }
     if (interaction.commandName === 'hello') {
-        interaction.reply(`Hello ${interaction.user.username}`)
+        const user = interaction.options.getUser('user') || interaction.user
+        const text = interaction.options.getString('text') || ''
+        interaction.reply(`Hello ${user.username} ${text}`)
+    }
+    if (interaction.commandName === 'echo') {
+        const text = interaction.options.getString('text')
+        interaction.reply(text)
     }
 
 
