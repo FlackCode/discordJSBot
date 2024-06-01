a4d038a868159d9c7d6f1cc5b3e4382ce2ceaaa3
flack <163582560+FlackCode@users.noreply.github.com>
2024-06-01 09:08:57 +0200

    Events & Event Handler

--- bot.js
+++ bot.js
@@ -1,54 +1,26 @@
-import { Client, Collection, GatewayIntentBits, SlashCommandBuilder } from 'discord.js'
+import { Client, Collection, GatewayIntentBits } from 'discord.js'
 require('dotenv').config()
-const client = new Client({ intents: [GatewayIntentBits.Guilds] })
+const client = new Client(
+  { 
+    intents: [GatewayIntentBits.Guilds] | 
+    [GatewayIntentBits.GuildMembers] | 
+    [GatewayIntentBits.MessageContent] | 
+    [GatewayIntentBits.GuildMessages] 
+  })
 const fs = require('node:fs')
+const path = require('node:path')
 
-client.commands = getCommands('./commands')
+const eventsPath = path.join(__dirname, 'events')
+const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'))
 
-client.on('ready', (c) => {
-  console.log(`Started as ${c.user.tag}`)
-})
-
-client.on('interactionCreate', interaction => {
-  if (!interaction.isChatInputCommand) return
-  console.log(client.commands)
-  let command = client.commands.get(interaction.commandName)
-  try {
-    if (interaction.replied) return
-    command.execute(interaction)
-  } catch (error) {
-    console.error(error)
+for (const file of eventFiles) {
+  const filePath = path.join(eventsPath, file)
+  const event = require(filePath)
+  if (event.once) {
+    client.once(event.name, (...args) => event.execute(...args))
+  } else {
+    client.on(event.name, (...args) => event.execute(...args))
   }
-})
+}
 
 client.login(process.env.TOKEN)
-
-function getCommands(dir) {
-  let commands = new Collection()
-  const commandFiles = getFiles(dir)
-
-  for (const commandFile of commandFiles) {
-    const command = require(commandFile)
-    commands.set(command.data.toJSON().name, command)
-  }
-  return commands
-}
-
-function getFiles(dir) {
-  const files = fs.readdirSync(dir, {
-      withFileTypes: true
-  })
-  let commandFiles = []
-
-  for (const file of files) {
-      if (file.isDirectory()) {
-          commandFiles = [
-              ...commandFiles,
-              ...getFiles(`${dir}/${file.name}`)
-          ]
-      } else if (file.name.endsWith('.js')) {
-          commandFiles.push(`${dir}/${file.name}`)
-      }
-  }
-  return commandFiles
-}
